0c8bf60d0ba858ca1aebd6243bf5418c93e71044
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-10-20 10:43:41 -0400

    Configurando petición HTTP a Giphy para traer los gifs a través del módulo HttpClientModule

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private apiKey    : string   = 'DBhtkKwAMsLASKZZdTaTgZFUCIeZ239u';
   private _historial: string[] = [];
 
   get historial() {
     return [...this._historial];
   }
+
+  constructor( private http: HttpClient ) {}
 
   buscarGifs( query: string = '' ) {
 
@@ -20,6 +24,10 @@ export class GifsService {
       this._historial = this._historial.splice(0,10);
     }
 
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=DBhtkKwAMsLASKZZdTaTgZFUCIeZ239u&q=dragon ball z&limit=10')
+      .subscribe( ( resp: any ) => {
+        console.log( resp.data );
+      })
 
     console.log( this._historial );
   }
