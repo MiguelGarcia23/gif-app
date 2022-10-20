908b6e7d84cbc9aa6a8c6d88f156b54d8dc7bfa1
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-10-20 11:52:05 -0400

    Colocando un tipado a las peticiones http con Quicktype

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { Gif, SearchGifsResponse } from '../interface/gif.interface';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +12,7 @@ export class GifsService {
   private _historial: string[] = [];
 
   // TODO: Cambiar any por su tipo
-  public resultados: any[] = [];
+  public resultados: Gif[] = [];
 
   get historial() {
     return [...this._historial];
@@ -27,8 +29,8 @@ export class GifsService {
       this._historial = this._historial.splice(0,10);
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=DBhtkKwAMsLASKZZdTaTgZFUCIeZ239u&q=${ query }&limit=10`)
-      .subscribe( ( resp: any ) => {
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=DBhtkKwAMsLASKZZdTaTgZFUCIeZ239u&q=${ query }&limit=10`)
+      .subscribe( ( resp ) => {
         console.log( resp.data );
         this.resultados = resp.data;
       })
