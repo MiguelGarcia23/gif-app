b31f159256e44de4e7f4f2b07935989a464a6a47
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-10-20 12:43:21 -0400

    Simplificando la línea del pedido http a través del uso de HttpParams

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { Gif, SearchGifsResponse } from '../interface/gif.interface';
 
@@ -8,8 +8,9 @@ import { Gif, SearchGifsResponse } from '../interface/gif.interface';
 })
 export class GifsService {
 
-  private apiKey    : string   = 'DBhtkKwAMsLASKZZdTaTgZFUCIeZ239u';
-  private _historial: string[] = [];
+  private apiKey     : string   = 'DBhtkKwAMsLASKZZdTaTgZFUCIeZ239u';
+  private servicioUrl: string   = 'https://api.giphy.com/v1/gifs';
+  private _historial : string[] = [];
 
   public resultados: Gif[] = [];
 
@@ -39,7 +40,12 @@ export class GifsService {
       localStorage.setItem( 'historial', JSON.stringify( this._historial ) );
     }
 
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=DBhtkKwAMsLASKZZdTaTgZFUCIeZ239u&q=${ query }&limit=10`)
+    const params = new HttpParams()
+          .set('api_key', this.apiKey)
+          .set('limit', '10')
+          .set('q', query);
+
+    this.http.get<SearchGifsResponse>(`${ this.servicioUrl }/search`, { params })
       .subscribe( ( resp ) => {
         console.log( resp.data );
         this.resultados = resp.data;
